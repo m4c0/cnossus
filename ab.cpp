ec50f8af949547a37d84569746a4fb223c63c324
Eduardo Costa <m4c0@users.noreply.github.com>
2023-05-14 15:04:23 +0100

    writing as PPM for testability

--- ab.cpp
+++ ab.cpp
@@ -8,6 +8,12 @@ constexpr const auto tile_side = 32;
 constexpr const auto tile_stride = 16;
 constexpr const auto atlas_width = tile_side * tile_stride;
 
+struct rgb {
+  int r;
+  int g;
+  int b;
+};
+
 int main(int argc, char **argv) {
   if (argc != 2) {
     std::cerr << "Usage: " << argv[0] << " <output-ppm>\n";
@@ -18,6 +24,8 @@ int main(int argc, char **argv) {
     std::cerr << "Could not open file: " << argv[1] << "\n";
     return 1;
   }
+
+  rgb pixies[atlas_width][atlas_width]{};
 
   int w;
   int h;
@@ -33,6 +41,14 @@ int main(int argc, char **argv) {
     std::cerr << "Expecting tiles to be a square of " << tile_side << "x"
               << tile_side << "\n";
     return 1;
+  }
+
+  f << "P3\n" << atlas_width << " " << atlas_width << " 255\n";
+  for (auto y = 0; y < atlas_width; y++) {
+    for (auto x = 0; x < atlas_width; x++) {
+      auto &c = pixies[y][x];
+      f << c.r << " " << c.g << " " << c.b << "\n";
+    }
   }
 
   return 0;
