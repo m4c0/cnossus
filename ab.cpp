2d4f07ba90e0bc4a858870173b8c7d5a9655373b
Eduardo Costa <m4c0@users.noreply.github.com>
2023-05-14 15:14:16 +0100

    reusable image stamp

--- ab.cpp
+++ ab.cpp
@@ -15,33 +15,23 @@ struct rgb {
   int a;
 };
 
-int main(int argc, char **argv) {
-  if (argc != 2) {
-    std::cerr << "Usage: " << argv[0] << " <output-ppm>\n";
-    return 1;
-  }
-  std::ofstream f{argv[1]};
-  if (!f) {
-    std::cerr << "Could not open file: " << argv[1] << "\n";
-    return 1;
-  }
+rgb pixies[atlas_width][atlas_width]{};
 
-  rgb pixies[atlas_width][atlas_width]{};
-
+bool stamp(const char *fname, char id) {
   int w;
   int h;
   int n;
-  auto data = stbi_load("atlas/adze.png", &w, &h, &n, 4);
+  auto data = stbi_load(fname, &w, &h, &n, 4);
   if (data == nullptr) {
     std::cerr << "Failure reading image"
               << "\n";
-    return 1;
+    return false;
   }
 
   if (w != h && w != tile_side) {
     std::cerr << "Expecting tiles to be a square of " << tile_side << "x"
               << tile_side << "\n";
-    return 1;
+    return false;
   }
 
   for (auto y = 0; y < tile_side; y++) {
@@ -57,6 +47,23 @@ int main(int argc, char **argv) {
       };
     }
   }
+
+  return true;
+}
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <output-ppm>\n";
+    return 1;
+  }
+  std::ofstream f{argv[1]};
+  if (!f) {
+    std::cerr << "Could not open file: " << argv[1] << "\n";
+    return 1;
+  }
+
+  if (!stamp("atlas/adze.png", '0'))
+    return 1;
 
   f << "P3\n" << atlas_width << " " << atlas_width << " 255\n";
   for (auto y = 0; y < atlas_width; y++) {
