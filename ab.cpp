fa21abc2ffb22786546a862ee00c23fb7994ef70
Eduardo Costa <m4c0@users.noreply.github.com>
2023-05-14 14:46:32 +0100

    basic image read with STB

--- ab.cpp
+++ ab.cpp
@@ -1,5 +1,12 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 #include <fstream>
 #include <iostream>
+
+constexpr const auto tile_side = 32;
+constexpr const auto tile_stride = 16;
+constexpr const auto atlas_width = tile_side * tile_stride;
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -9,6 +16,22 @@ int main(int argc, char **argv) {
   std::ofstream f{argv[1]};
   if (!f) {
     std::cerr << "Could not open file: " << argv[1] << "\n";
+    return 1;
+  }
+
+  int w;
+  int h;
+  int n;
+  auto data = stbi_load("atlas/adze.png", &w, &h, &n, 4);
+  if (data == nullptr) {
+    std::cerr << "Failure reading image"
+              << "\n";
+    return 1;
+  }
+
+  if (w != h && w != tile_side) {
+    std::cerr << "Expecting tiles to be a square of " << tile_side << "x"
+              << tile_side << "\n";
     return 1;
   }
 
