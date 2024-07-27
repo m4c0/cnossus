c36e64554a8fe6bbeece33ea5512df5c57d74a0d
Eduardo <m4c0@users.noreply.github.com>
2024-07-27 10:37:21 +0100

    minor refactoring in attack modal

--- cno_player_turn.cpp
+++ cno_player_turn.cpp
@@ -7,34 +7,30 @@ static constexpr const float anim_length = 100;
 
 static dotz::ivec2 target{};
 static sitime::stopwatch timer{};
-static void (*on_end)();
-static void (*on_frame)();
 
 static void redraw() {
   quack::donald::data([](auto all) { return qsu::draw(all, play::draw); });
-
-  if (timer.millis() > anim_length) {
-    on_end();
-    cno::modes::game();
-  } else {
-    on_frame();
-  }
 }
 
 void cno::modes::player_turn::attack(dotz::ivec2 tgt) {
   timer = {};
   target = tgt;
 
-  on_end = [] { player::d.anim_coord = player::d.coord; };
-  on_frame = [] {
+  using namespace casein;
+  reset_k(KEY_DOWN);
+  handle(REPAINT, [] {
+    redraw();
+
+    if (timer.millis() > anim_length) {
+      player::d.anim_coord = player::d.coord;
+      cno::modes::game();
+      return;
+    }
+
     float dt = timer.millis() / anim_length;
     if (dt > 0.5)
       dt = 1.0 - dt;
     dt *= 0.5;
     player::d.anim_coord = dotz::mix(player::d.coord, target, dt);
-  };
-
-  using namespace casein;
-  reset_k(KEY_DOWN);
-  handle(REPAINT, redraw);
+  });
 }
