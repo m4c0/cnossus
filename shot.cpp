ad19ce7542d06289eaa46e91aff397d9c6f65f14
Eduardo Costa <m4c0@users.noreply.github.com>
2024-10-25 17:32:56 +0100

    shot: adding some voo things

--- shot.cpp
+++ shot.cpp
@@ -8,6 +8,14 @@ import play;
 import qsu;
 import quack;
 import save;
+import vee;
+import voo;
+
+void shot(vee::physical_device pd, vee::extent ext) {
+  voo::offscreen::colour_buffer m_colour { pd, ext };
+  voo::offscreen::depth_buffer m_depth { pd, ext };
+  voo::offscreen::host_buffer m_host { pd, ext };
+}
 
 struct init {
   init() {
@@ -16,7 +24,7 @@ struct init {
     app_name("cnossus");
     max_quads(map::width * map::height * 2 + 1024);
 
-    clear_colour({0, 0, 0, 1});
+    clear_colour({ 0, 0, 0, 1 });
     atlas(qsu::atlas);
 
     save::read([] {}, [] {});
